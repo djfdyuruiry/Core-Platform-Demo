35528b36985eab525aa80bf8b780a7fb731e878e
Jaime Still <jpstill85@gmail.com>
2017-03-13 22:47:00 -0400

    sidebar integration

--- src/app/components/app/app.component.ts
+++ src/app/components/app/app.component.ts
@@ -1,8 +1,16 @@
 import { Component } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
     selector: 'app',
     templateUrl: './app.component.html',
     styleUrls: ['app.component.css']
 })
-export class AppComponent { }
+export class AppComponent {
+    toggleSidebar() {
+        $('.ui.sidebar').sidebar({
+            context: $('app'),
+            transition: 'overlay'
+        }).sidebar('toggle');
+    }
+}
--- src/boot-client.ts
+++ src/boot-client.ts
@@ -1,9 +1,11 @@
 import 'angular2-universal-polyfills/browser';
+import 'semantic-ui-css';
 import { enableProdMode, PlatformRef } from '@angular/core';
 import { platformUniversalDynamic } from 'angular2-universal';
 import { AppModule } from './app/app.module';
-import 'semantic-ui-css';
-const rootElemTagName: string = 'app'; // update this if you change your root component selector
+
+const rootElemTagName = 'app'; // update this if you change your root component selector
+const platform: PlatformRef = platformUniversalDynamic();
 
 // enable either Hot Module Reloading or production mode
 if (module['hot']) {
@@ -20,7 +22,6 @@ if (module['hot']) {
 }
 
 // boot the application, either now or when the DOM content is loaded
-const platform: PlatformRef = platformUniversalDynamic();
 const bootApplication = () => { platform.bootstrapModule(AppModule); };
 if (document.readyState === 'complete') {
     bootApplication();
